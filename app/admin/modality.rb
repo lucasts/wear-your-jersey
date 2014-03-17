edf551a848846908b1576d1d00728ed69661bc01
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-03-16 23:04:56 -0300

    Add html_editor on description field of modality admin session

--- app/admin/modality.rb
+++ app/admin/modality.rb
@@ -7,22 +7,31 @@ ActiveAdmin.register Modality do
     end
   end
 
+  filter :title, :as => :string
+  
   sortable
 
   index do
-    selectable_column
     sortable_handle_column
+    selectable_column
     column :title
-    column :description
+    #column :description
     actions
   end
 
-  filter :title, :as => :string
+  show do |modality|
+    attributes_table do
+      row :title
+      row :description do
+        modality.description.html_safe
+      end
+    end
+  end
 
   form do |f|
     f.inputs do
       f.input :title
-      f.input :description
+      f.input :description, as: :html_editor
     end
     f.actions
   end
