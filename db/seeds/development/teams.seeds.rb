8077b5b6d34128ea642ab2be652d8c38b2758f59
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-03-18 18:44:10 -0300

    Fixing the teams names on development seed

--- db/seeds/development/teams.seeds.rb
+++ db/seeds/development/teams.seeds.rb
@@ -2,8 +2,8 @@ after "development:modalities" do
   if Team.count.zero?
     puts "Seed: Creating Teams to modalities"
     Modality.all.each do |modality|
-      FactoryGirl.create(:team, title: "Pricipal", modality: modality)
-      FactoryGirl.create(:team, title: "Academy", modality: modality)
+      FactoryGirl.create(:team, title: "First Team", modality: modality)
+      FactoryGirl.create(:team, title: "Academy Team", modality: modality)
     end
   end
 end
