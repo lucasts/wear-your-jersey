5cd7675036f81dd5fdcf2d7528c9a2863f3ed8ca
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-03-23 23:12:35 -0300

    Syntax improve on specs descriptions

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,14 +1,12 @@
 require "spec_helper"
 
 describe Team do
-  it "Team Factory is valid" do
-    expect(FactoryGirl.build(:team)).to be_valid
+  describe "association" do
+    it { should belong_to :modality }
   end
 
-  it "Title and description are required fields" do
-    team_to_test = FactoryGirl.build(:team, title: nil, description: nil)
-    expect(team_to_test).to_not be_valid
-    expect(team_to_test).to have(1).errors_on(:title)
-    expect(team_to_test).to have(1).errors_on(:description)
+  describe "validation of required fields (title, description)" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
   end
 end
