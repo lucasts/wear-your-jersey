6d66e3c304189c36bde56c793d81eb2cc318e3d8
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-03-28 22:46:39 -0300

    Add players migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321011710) do
+ActiveRecord::Schema.define(version: 20140324225958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,26 @@ ActiveRecord::Schema.define(version: 20140321011710) do
     t.text    "description"
     t.integer "position"
   end
+
+  create_table "players", force: true do |t|
+    t.integer  "team_id"
+    t.integer  "position_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "nickname"
+    t.integer  "position"
+    t.string   "player_image_file_name"
+    t.string   "player_image_content_type"
+    t.integer  "player_image_file_size"
+    t.datetime "player_image_updated_at"
+    t.integer  "number"
+    t.date     "birth"
+    t.string   "hometown"
+    t.text     "bio"
+  end
+
+  add_index "players", ["position_id"], name: "index_players_on_position_id", using: :btree
+  add_index "players", ["team_id"], name: "index_players_on_team_id", using: :btree
 
   create_table "positions", force: true do |t|
     t.integer "modality_id"
