a0026339b465d6623b51eff5f3dc0b9fdd4caa0e
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-03-29 12:03:54 -0300

    Add nickname to required fields

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,14 @@
 class Player < ActiveRecord::Base
   acts_as_list
 
+  attr_accessor :nickname
+
   belongs_to :team
   belongs_to :position
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :nickname, presence: true
 
   has_attached_file :player_image,
     :styles => { :thumb => "100x>", :small => "350x>" }
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -9,5 +9,6 @@ describe Player do
   describe "validation of required fields (first_name, nickname)" do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
+    it { should validate_presence_of :nickname }
   end
 end
