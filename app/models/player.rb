3ae1a31e33a8fede37ff3c7ffcf4b83d3d07b55f
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-03-29 10:50:25 -0300

    Add player model

---
+++ app/models/player.rb
@@ -0,0 +1,14 @@
+class Player < ActiveRecord::Base
+  acts_as_list
+
+  belongs_to :team
+  belongs_to :position
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  has_attached_file :player_image,
+    :styles => { :thumb => "100x>", :small => "350x>" }
+  validates_attachment_content_type :player_image,
+    :content_type => ["image/jpg", "image/jpeg", "image/png"]
+end
