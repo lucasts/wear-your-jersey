8dfd6085bf5a5332ca78a6dd550883fd3196c53c
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-16 00:09:27 -0300

    Move the test of full_name_and_id method to decorator spec

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -5,17 +5,8 @@ describe Team do
     it { should belong_to :modality }
   end
 
-  describe "validation of required fields (title, description)" do
+  describe "validation of required fields" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
-  end
-
-  describe "full_name_and_id method" do
-    team  = FactoryGirl.create(:team)
-    title = [team.modality.title, team.title].join " - "
-    id    = team.id
-    it "should return a array with full title and id" do
-      expect(team.full_name_and_id).to eq [title, id]
-    end
   end
 end
