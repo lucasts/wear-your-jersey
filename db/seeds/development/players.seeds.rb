4ef4c7899d4b3c46087aaafc0394346112c92e7c
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-17 00:56:51 -0300

    Add players seed

---
+++ db/seeds/development/players.seeds.rb
@@ -0,0 +1,11 @@
+after "development:roles", "development:teams" do
+  if Player.count.zero?
+    puts "Seed: Creating players to teams"
+    Team.all.each do |team|
+      10.times do
+        role = team.modality.roles.sample
+        FactoryGirl.create(:player, role: role)
+      end
+    end
+  end
+end
--- db/seeds/development/teams.seeds.rb
+++ db/seeds/development/teams.seeds.rb
@@ -1,6 +1,6 @@
 after "development:modalities" do
   if Team.count.zero?
-    puts "Seed: Creating Teams to modalities"
+    puts "Seed: Creating teams to modalities"
     Modality.all.each do |modality|
       FactoryGirl.create(:team, title: "First Team", modality: modality)
       FactoryGirl.create(:team, title: "Academy Team", modality: modality)
