f30793edfc1f5bc1a72a72feadf3b0fb5e7d2941
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-27 15:14:42 -0300

    Add sponsor_image on sponsor model

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -2,4 +2,9 @@ class Sponsor < ActiveRecord::Base
   belongs_to :sponsor_category
   validates :title, presence: true
   acts_as_list
+
+  has_attached_file :sponsor_image,
+    :styles => { :thumb => "100x>", :small => "350x>" }
+  validates_attachment_content_type :sponsor_image,
+    :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
