8ef3e4fc3c12f2c68e0c45ca4a6d910ab528617e
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-27 18:16:19 -0300

    Change image path used on data factories

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,10 +2,10 @@ require "faker"
 
 FactoryGirl.define do
   factory :team do
-    team_image_path = Rails.root.join("spec", "fixtures", "team")
+    image_path = Rails.root.join("spec", "fixtures", "image")
     title       { Faker::Lorem.word }
     description { Faker::Lorem.sentence(20, true) }
-    team_image  { File.new(team_image_path.join("team.jpg")) }
+    team_image  { File.new(image_path.join("image.png")) }
     modality
   end
 end
