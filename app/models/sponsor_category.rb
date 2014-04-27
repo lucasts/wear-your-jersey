868dfae22ca9dbdfc4bf26dd8c2ec45f5dd370d5
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-27 14:53:24 -0300

    Add association with sponsor on sponsor category model

--- app/models/sponsor_category.rb
+++ app/models/sponsor_category.rb
@@ -1,5 +1,7 @@
 class SponsorCategory < ActiveRecord::Base
   has_enumeration_for :grandeur, :with => SponsorCategoryGrandeurType, :create_helpers => true
+  has_many :sponsors
+
   validates :title, presence: true
   validates :grandeur, presence: true
   acts_as_list
