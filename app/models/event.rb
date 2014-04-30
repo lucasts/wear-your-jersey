881d613a2204c4027de9bddb7f4f96ae2324f7d3
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-29 23:57:05 -0300

    Fix association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  belongs_to :teams
+  belongs_to :team
   validates :team_id, presence: true
   validates :locale , presence: true
   validates :date   , presence: true
