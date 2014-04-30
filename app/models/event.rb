70c9404240ef584856657db3c11dd0c775dae47f
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-29 23:50:13 -0300

    Add validations to events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :teams
-  validates :locale, presence: true
-  validates :date  , presence: true
-  validates :time  , presence: true
+  validates :team_id, presence: true
+  validates :locale , presence: true
+  validates :date   , presence: true
+  validates :time   , presence: true
 end
