68f616ab8b7c430b622822f9b833c66dc1d5fd03
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-04-29 23:50:47 -0300

    Add event on activeadmin

---
+++ app/admin/event.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register Event do
+
+  controller do
+    def permitted_params
+      params.permit event: [:team_id, :locale, :description,
+                            :opponent, :date, :time]
+    end
+  end
+
+  filter :team
+  filter :locale
+  filter :opponent
+  filter :date
+
+  index do
+    selectable_column
+    column :team
+    column :locale
+    column :opponent
+    column :date
+    actions
+  end
+
+
+
+  form do |f|
+    f.inputs do
+      f.input :team_id,
+        :as         => :select,
+        :collection => TeamDecorator.
+                         decorate_collection(Team.all).map { |team| team.full_name_and_id }
+      f.input :locale
+      f.input :description, :as => :html_editor
+      f.input :opponent
+      f.input :date, :as => :datepicker
+      f.input :time
+    end
+    f.actions
+  end
+end
