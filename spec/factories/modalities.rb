551a4ec5df6d855378e400773963cc57659ce8c7
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-05-03 12:15:08 -0300

    Remove have_opponents_on_practice of modalities factory

--- spec/factories/modalities.rb
+++ spec/factories/modalities.rb
@@ -2,8 +2,7 @@ require "faker"
 
 FactoryGirl.define do
   factory :modality do
-    title                      { Faker::Lorem.word }
-    description                { Faker::Lorem.sentence(20, true) }
-    have_opponents_on_practice { [true, false].sample }
+    title       { Faker::Lorem.word }
+    description { Faker::Lorem.sentence(20, true) }
   end
 end
