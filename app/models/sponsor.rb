6c9e311fcbb1e128dfd8f94b0e6582b238707b5f
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-05-09 23:13:23 -0300

    Add master_sponsors scope to sponsors model

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -7,4 +7,7 @@ class Sponsor < ActiveRecord::Base
     :styles => { :thumb => "100x>", :small => "350x>" }
   validates_attachment_content_type :sponsor_image,
     :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  scope :master_sponsors, -> { joins(:sponsor_category).
+    where('sponsor_categories.grandeur' => SponsorCategoryGrandeurType::MASTER_SPONSOR) }
 end
