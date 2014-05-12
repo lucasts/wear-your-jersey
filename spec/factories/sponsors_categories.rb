bf5b5b82c5f051f027ba22cdc5d729236469a876
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-05-12 00:27:21 -0300

    Improve on sponsors seeds

--- spec/factories/sponsors_categories.rb
+++ spec/factories/sponsors_categories.rb
@@ -2,7 +2,7 @@ require "faker"
 
 FactoryGirl.define do
   factory :sponsor_category do
-    title    { Faker::Lorem.word }
+    title    { Faker::Lorem.word.capitalize }
     grandeur { (1..SponsorCategoryGrandeurType.length).to_a.sample }
   end
 end
