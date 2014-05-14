a3b1d97a3af8bcdc9773a841a0b848ca034d3c12
viniciusalmeida <viniciusalmeidadasilva@ymail.com>
2014-05-14 18:30:10 -0300

    Improve the db consult to get all sponsors

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,9 +1,6 @@
 class SponsorsController < ApplicationController
   def index
-    @categories_sponsors = SponsorCategory
-      .categories_by_grandeur(SponsorCategoryGrandeurType::SPONSOR)
-    @categories_partners = SponsorCategory
-      .categories_by_grandeur(SponsorCategoryGrandeurType::PARTNER)
+    @categories = SponsorCategory.all_categories
   end
 
   def show
--- app/models/sponsor_category.rb
+++ app/models/sponsor_category.rb
@@ -2,13 +2,17 @@ class SponsorCategory < ActiveRecord::Base
   has_enumeration_for :grandeur, with: SponsorCategoryGrandeurType, create_helpers: true
   has_many :sponsors
 
-  validates :title, presence: true
-  validates :grandeur, presence: true
+  validates_presence_of :title, :grandeur
   acts_as_list
 
   scope :categories_by_grandeur, ->(grandeur) {
     includes(:sponsors)
     .where(grandeur: grandeur)
     .order('sponsor_categories.position', 'sponsors.position')
+  }
+
+  scope :all_categories, -> {
+    includes(:sponsors)
+    .order('sponsor_categories.grandeur', 'sponsor_categories.position', 'sponsors.position')
   }
 end
